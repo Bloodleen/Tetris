31371796e0d8a98ea753dda025b01a65d9927074
Mirai <mariamzemskova@mail.ru>
2023-06-26 19:45:15 +0300

    create class dot

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -1,27 +1,19 @@
-﻿internal class Program
-{
-    static void Draw(int x, int y, char c)
-    {
-        Console.SetCursorPosition(x, y);
-        Console.Write(c);
-    }
+﻿using System.Drawing;
+using Tetris;
 
+internal class Program
+{
     private static void Main(string[] args)
     {
         Console.SetWindowSize(40, 30);
         Console.SetBufferSize(40, 30);
 
-        int x1 = 2;
-        int y1 = 3;
-        char c1 = '*';
+        dot p1 = new dot();
+        p1.x = 2;
+        p1.y = 3;
+        p1.c = '*';
+        p1.Draw();
 
-        Draw(x1, y1, c1);
-
-        int x2 = 2;
-        int y2 = 3;
-        char c2 = '#';
-
-        Draw(x2, y2, c2);
 
         Console.ReadLine();
     }
